bc5dc171e20447082ab5f7686f2f97ee2d87be34
SuicaLondon <suicadevinlondon@gmail.com>
2025-08-13 23:52:36 +0100

    feat: implement the next bill day function

--- src/lib/features/duration/components/copy-all-durations-button/index.tsx
+++ src/lib/features/duration/components/copy-all-durations-button/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { memo } from "react";
 import { useDurationStore } from "../../stores/duration.store";
 
-export const CopyAllDurationsButton = () => {
+export const CopyAllDurationsButton = memo(function CopyAllDurationsButton() {
   const copyAllWidgets = useDurationStore((state) => state.copyAllWidgets);
   return (
     <Button variant="outline" onClick={copyAllWidgets}>
       Copy Durations
     </Button>
   );
-};
+});
